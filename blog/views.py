66e4dec9fea8f29b449030218ca4e87bc129bfb8
yiruhannone <elva.yiruhan@gmail.com>
2026-04-22 10:19:16 +0200

    My Django Girls app, first commit

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post  # 引入你定义的模型
+
+def post_list(request):
+    # 这一行是核心：去数据库里拿所有发布过的帖子，并按发布时间排序
+    posts = Post.objects.all()
+    # 把拿到的数据（posts）塞进那个大括号里，传给 HTML
+    return render(request, 'blog/post_list.html', {'posts': posts})
