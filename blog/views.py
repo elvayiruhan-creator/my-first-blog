d1a567a2d08d3d430cf3c46934289954db2b2ffa
yiruhannone <elva.yiruhan@gmail.com>
2026-04-27 23:02:52 +0200

    Added post detail page

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,6 @@ def post_list(request):
     
     # 把拿到的数据 (posts) 塞进那个大括号里，传给 HTML
     return render(request, 'blog/post_list.html', {'posts': posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
